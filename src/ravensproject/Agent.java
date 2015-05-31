9fad44a70f8792f3907623dd310780adbeee1220
zhang3r <zhang3r@gatech.edu>
2015-05-31 00:49:15 -0400

    relationship generation code

--- src/ravensproject/Agent.java
+++ src/ravensproject/Agent.java
@@ -1,6 +1,7 @@
 package ravensproject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +63,11 @@ public class Agent {
 	 * @return your Agent's answer to this problem
 	 */
 	public int Solve(RavensProblem problem) {
+		// Algorithm
+		// 1. construct matrix
+		// 2. findDifference of all the answer choices
+		// - returns a relationship
+		// 3. see which difference works.
 		System.out
 				.println("/*************************solver started***************************************/");
 		System.out.println("problem Name: " + problem.getName());
@@ -69,6 +75,9 @@ public class Agent {
 		int matrixSize = panels == 3 ? 2 : 3;
 		RavensFigure[][] matrix = new RavensFigure[matrixSize][matrixSize];
 		int choices = panels == 3 ? 6 : 8;
+		boolean[] validChoices = new boolean[choices];
+		int[] scores = new int[choices];
+		Arrays.fill(scores, -1);
 		HashMap<String, RavensFigure> figures = problem.getFigures();
 
 		char panelSelection = 'A';
@@ -83,223 +92,147 @@ public class Agent {
 					.valueOf((char) (panelSelection + i)).toString());
 		}
 		/************************************** generate and test ****************************************/
-		/*
-		 * O X ? Y ? ? ? ? ?
-		 * 
-		 * 
-		 * O ? ? ?
-		 */
 		// only generate possible relationships
-		/***************** relationships across ********************/
-		// find difference in attributes in a list of RavensFigures
-		for (int x = 0; x < matrixSize - 1; x++) {
-			// each column's change needs to be the same.
-			List<RavensFigure> list = new ArrayList<>();
-			for (int y = 0; y < matrixSize; y++) {
-				list.add(matrix[x][y]);
-			}
-			// find difference
-			// relationship for row
+		/***************** relationships down ********************/
 
-		}
-
-		// generate ways of achieve that difference
-
-		/***************** relationships down **********************/
-		// find difference in attributes
-		// generate ways of achieve that difference
+		List<List<Map<String, CHANGE>>> differenceY = null;
+		int x = matrixSize - 1;
+		List<RavensFigure> listY = new ArrayList<>();
 		for (int y = 0; y < matrixSize - 1; y++) {
-			List<RavensFigure> list = new ArrayList<>();
-			for (int x = 0; x < matrixSize; x++) {
-				list.add(matrix[x][y]);
-			}
-			// find relationship
-			// relationship for column
-			// relationshipY = findRelationship(list);
-
+			listY.add(matrix[x][y]);
 		}
+		differenceY = generateRelationship(listY);
+
+		/***************** relationships across **********************/
+
+		List<List<Map<String, CHANGE>>> differenceX = null;
+		int y = matrixSize - 1;
+		List<RavensFigure> listX = new ArrayList<>();
+		for (x = 0; x < matrixSize - 1; x++) {
+			listX.add(matrix[x][y]);
+		}
+
+		differenceX = generateRelationship(listX);
 		// TEST
+		// TEST transformation
 		// for all answers see if there is at least one answer that passes the
-		// relationship
 
-		for (String key : figures.keySet()) {
-			System.out
-					.println("######################Figure###############################");
-			System.out.println("Raven Figure: " + key);
-			RavensFigure figure = figures.get(key);
+		for (int i = 1; i <= choices; i++) {
+			// add answer choice to last element
+			listY.add(figures.get(i + ""));
+			listX.add(figures.get(i + ""));
+			differenceY = generateRelationship(listY);
+			differenceX = generateRelationship(listX);
 
-			System.out.println("figure name: " + figure.getName());
+			// TEST answer choice
+			boolean isPossibleX = testReleationship("x", differenceX, matrix);
+			boolean isPossibleY = testReleationship("y", differenceY, matrix);
+			validChoices[i - 1] = isPossibleX && isPossibleY;
+			scores[i - 1] = calculateScore(differenceX)
+					+ calculateScore(differenceY);
 
-			// construct a matrix
-			// 3. use generate and test for valid relations
-			// 4. use means-ends analysis to find optimal answer
-			System.out.println("figure visual: " + figure.getVisual());
-
-			for (String objKey : figure.getObjects().keySet()) {
-				System.out
-						.println("-------------------- objects ----------------------");
-				System.out.println("Raven Obj: " + objKey);
-				RavensObject obj = figure.getObjects().get(objKey);
-				System.out.println("Object: " + obj.getName());
-				for (String attrKey : obj.getAttributes().keySet()) {
-					System.out.println("attribute: " + attrKey);
-					System.out.println("attribute value: "
-							+ obj.getAttributes().get(attrKey));
-				}
-
-			}
+			// removing last element
+			listY.remove(listY.size() - 1);
+			listX.remove(listX.size() - 1);
 
 		}
-		return -1;
+		int result = -1;
+		int resultIndex = -1;
+		for (int z = 0; z < scores.length; z++) {
+			if (validChoices[z] && scores[z] > result) {
+				result = scores[z];
+				resultIndex = z;
+			}
+		}
+
+		return resultIndex;
+
+		// // return top choices
+		// // relationship
+		//
+		// for (String key : figures.keySet()) {
+		// System.out
+		// .println("######################Figure###############################");
+		// System.out.println("Raven Figure: " + key);
+		// RavensFigure figure = figures.get(key);
+		//
+		// System.out.println("figure name: " + figure.getName());
+		//
+		// // construct a matrix
+		// // 3. use generate and test for valid relations
+		// // 4. use means-ends analysis to find optimal answer
+		// System.out.println("figure visual: " + figure.getVisual());
+		//
+		// for (String objKey : figure.getObjects().keySet()) {
+		// System.out
+		// .println("-------------------- objects ----------------------");
+		// System.out.println("Raven Obj: " + objKey);
+		// RavensObject obj = figure.getObjects().get(objKey);
+		// System.out.println("Object: " + obj.getName());
+		// for (String attrKey : obj.getAttributes().keySet()) {
+		// System.out.println("attribute: " + attrKey);
+		// System.out.println("attribute value: "
+		// + obj.getAttributes().get(attrKey));
+		// }
+		//
+		// }
+		//
+		// }
+
 	}
 
-	private void generateAndTest(List<HashMap<String, CHANGE>> changes,
-			RavensFigure lastFrame, List<RavensFigure> list, int[] answerScore) {
+	private boolean testReleationship(String string,
+			List<List<Map<String, CHANGE>>> differenceX, RavensFigure[][] matrix) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-		if (list == null || list.size() == 0) {
-			// TODO Test changes on last row and column of matrix.
-		} else {
-			RavensFigure figure = list.get(0);
+	// difference of all the frames
+	private List<List<Map<String, CHANGE>>> generateRelationship(
+			List<RavensFigure> list) {
+		List<List<Map<String, CHANGE>>> difference = new ArrayList<>();
+		RavensFigure lastFrame = null;
+		for (RavensFigure figure : list) {
 			if (lastFrame == null) {
 				lastFrame = figure;
 			}
 
-			list.remove(0);
-			// Find difference
-			List<List<HashMap<String, CHANGE>>> differences = findDifference(
-					figure, lastFrame);
-			for (List<HashMap<String, CHANGE>> difference : differences) {
-
-				// update changes
-				List<HashMap<String, CHANGE>> newChanges = updateChange(changes, difference);
-				generateAndTest(newChanges, figure, list, answerScore);
-			}
+			difference.add(findDifference(figure, lastFrame));
 
 		}
-
-		return;
-		// List<Map<String, CHANGE>> change = new ArrayList<>();
-		// RavensFigure lastFrame = null;
-		// for (RavensFigure figure : list) {
-		// if (figure != null && figure.getObjects() != null) {
-		// // compare list size and keyset size.
-		// // if change is 0 initialize
-		// if (change.size() == 0) {
-		// lastFrame = figure;
-		// for (String objectKey : figure.getObjects().keySet()) {
-		// Map<String, CHANGE> attrChange = new HashMap<>();
-		// RavensObject obj = figure.getObjects().get(objectKey);
-		// for (String attrKey : obj.getAttributes().keySet()) {
-		// attrChange.put(attrKey, CHANGE.NO_CHANGE);
-		// }
-		// change.add(attrChange);
-		// }
-		// } else if (change.size() != figure.getObjects().keySet().size()) {
-		// // if size is different, then an addition or subtraction has
-		// // occurred
-		//
-		// } else {
-		// // if size is the same
-		// // for each object check attribute change
-		// }
-		// }
-		// }
-		// return change;
-
-		//
-		// for (String objectKey : figure.getObjects().keySet()) {
-		//
-		//
-		//
-		//
-		//
-		// HashMap<String, String> attributes = null;
-		// Map<String, CHANGE> attrChange =null;
-		// if(change.containsKey(objectKey)){
-		// attrChange = change.get(objectKey);
-		// }else{
-		//
-		// attrChange = new HashMap<>();
-		// }
-		// HashMap<String, String> temp = figure.getObjects()
-		// .get(objectKey).getAttributes();
-		// if (attributes == null) {
-		// attributes = temp;
-		// for (String attrKey : temp.keySet()) {
-		// attrChange.put(attrKey, CHANGE.NO_CHANGE);
-		// }
-		// }
-		// for (String attrKey : temp.keySet()) {
-		// // key was found
-
-		// if (attributes.containsKey(attrKey)) {
-		// if (!attributes.get(attrKey).equals(temp.get(attrKey))) {
-		// if (attrKey.equals("size")) {
-		// attrChange.put(attrKey, CHANGE.SCALED);
-		// } else if (attrKey.equals("angle")) {
-		// attrChange.put(attrKey, CHANGE.ROTATED);
-		// } else if (attrKey.equals("fill")) {
-		// attrChange.put(attrKey, CHANGE.FILL);
-		// } else if (attrKey.equals("shape")) {
-		// attrChange.put(attrKey, CHANGE.SHAPE);
-		// } else {
-		// attrChange.put(attrKey, CHANGE.TRANSLATION);
-		// }
-		// }
-		//
-		// } else {
-		// attributes.put(attrKey, temp.get(attrKey));
-		// attrChange.put(attrKey, CHANGE.ADDITION);
-		// }
-		//
-		// }
-		// // search for deletions
-		// for (String key : attributes.keySet()) {
-		// if (!temp.containsKey(key)) {
-		// attributes.remove(key);
-		// attrChange.put(key, CHANGE.DELETION);
-		//
-		// }
-		// }
-		// change.append(attrChange);
-		// }
-		// }
-		//
-		// return change;
+		return difference;
 	}
 
-	private List<HashMap<String, CHANGE>> updateChange(
-			List<HashMap<String, CHANGE>> changes,
-			List<HashMap<String, CHANGE>> difference) {
-		// TODO update changes
-		return null;
-	}
 	/**
-	 * return all the possible difference between the 2 frames
+	 * return all the difference between the 2 frames
+	 * 
 	 * @param figure
 	 * @param lastFrame
 	 * @return
 	 */
-	private List<List<HashMap<String, CHANGE>>> findDifference(
-			RavensFigure figure, RavensFigure lastFrame) {
-		//hashMap = possible change for object
-		//List<hashMap>= possible changes for frame
-		//list<list<hashmap>> = list of all possible changes for the frame
-		if(figure.getObjects().keySet().size()!=lastFrame.getObjects().keySet().size()){
-			//things got deleted/added
-			//delete enough objects from last frame to make sure the number of objects match.
-			
-		}else{
-			//number of object are the same
-			//see the difference between obj1 of figure and obj1 of last frame
-			//see the difference between obj2 of figure and obj2 of last frame
-			//etc.
+	private List<Map<String, CHANGE>> findDifference(RavensFigure figure,
+			RavensFigure lastFrame) {
+		List<Map<String, CHANGE>> frameDiff = new ArrayList<>();
+
+		// an object can be added/ removed or changed.
+		// hashMap = change for object
+		// List<hashMap>= possible changes for object
+
+		for (String figureKey : figure.getObjects().keySet()) {
+
+			RavensObject figureObject = figure.getObjects().get(figureKey);
+			for (String lastFrameKey : lastFrame.getObjects().keySet()) {
+				RavensObject lastFrameObject = lastFrame.getObjects().get(
+						lastFrameKey);
+
+				// compare objects
+				frameDiff.add(findObjDifference(figureObject, lastFrameObject));
+
+			}
+
 		}
-		
-		
-		// TODO find a list of differences
-		// TEMP only do differences if the object numbers dont add up
-		return null;
+
+		return frameDiff;
 	}
 
 	//
@@ -316,7 +249,108 @@ public class Agent {
 	// Shape unchanged
 	// 0 point
 
+	private Map<String, CHANGE> findObjDifference(RavensObject figureObject,
+			RavensObject lastFrameObject) {
+		Map<String, CHANGE> difference = new HashMap<>();
+		// this is a set intersect
+		String[] figureObjectAttrKey = (String[]) figureObject.getAttributes()
+				.keySet().toArray();
+		int figureObjAttrLength = figureObjectAttrKey.length;
+		String[] lastFrameObjectAttrKey = (String[]) lastFrameObject
+				.getAttributes().keySet().toArray();
+		List<String> attrFound = new ArrayList<>();
+		int lastFrameObjAttrLength = lastFrameObjectAttrKey.length;
+		for (int i = 0; i < lastFrameObjAttrLength; i++) {
+			for (int j = 0; j < figureObjAttrLength; j++) {
+				if (lastFrameObjectAttrKey[i].equals(figureObjectAttrKey[j])) {
+					// same attr
+					String attrKey = lastFrameObjectAttrKey[i];
+					CHANGE attrChange = CHANGE.NO_CHANGE;
+					if (!figureObject
+							.getAttributes()
+							.get(attrKey)
+							.equals(lastFrameObject.getAttributes()
+									.get(attrKey))) {
+
+						if (attrKey.equals("size")) {
+							attrChange = CHANGE.SCALED;
+						} else if (attrKey.equals("angle")) {
+							attrChange = CHANGE.ROTATED;
+						} else if (attrKey.equals("fill")) {
+							attrChange = CHANGE.FILL;
+						} else {
+							attrChange = CHANGE.TRANSLATION;
+						}
+
+					}
+					attrFound.add(attrKey);
+					difference.put(attrKey, attrChange);
+				}
+			}
+		}
+
+		for (String lf_attr : lastFrameObjectAttrKey) {
+			if (!attrFound.contains(lf_attr)) {
+				difference.put(lf_attr, CHANGE.DELETION);
+			}
+		}
+		for (String f_attr : figureObjectAttrKey) {
+			if (!attrFound.contains(f_attr)) {
+				difference.put(f_attr, CHANGE.ADDITION);
+			}
+		}
+
+		// the the same shape
+		// for all the objects in figure that wasnt selected, addition
+		// for all the object in last frame that wasn't selected, deletion
+		return difference;
+	}
+
+	private int calculateScore(List<List<Map<String, CHANGE>>> difference) {
+		int score = 0;
+		for (List<Map<String, CHANGE>> frame : difference) {
+			for (Map<String, CHANGE> object : frame) {
+				score += calculateScore(object);
+			}
+		}
+		return score;
+	}
+
+	private int calculateScore(Map<String, CHANGE> attributes) {
+		CHANGE[] changes = (CHANGE[]) attributes.values().toArray();
+		int score = 0;
+		//
+		// Unchanged
+		// 5 points
+		// Reflected
+		// 4 points
+		// Rotated
+		// 3 points
+		// Scaled
+		// 2 points
+		// Deleted
+		// 1 point
+		// Shape unchanged
+		// 0 point
+		for (CHANGE change : changes) {
+			if (change.equals(CHANGE.NO_CHANGE)) {
+				score += 5;
+			} else if (change.equals(CHANGE.REFLECTED)) {
+				score += 4;
+			} else if (change.equals(CHANGE.ROTATED)) {
+				score += 3;
+			} else if (change.equals(CHANGE.SCALED)
+					|| change.equals(CHANGE.FILL)) {
+				score += 2;
+			} else if (change.equals(CHANGE.ADDITION)
+					|| change.equals(CHANGE.DELETION)) {
+				score += 1;
+			}
+		}
+		return score;
+	}
+
 	enum CHANGE {
-		NO_CHANGE, REFLECTED, ROTATED, SCALED, SHAPE, FILL, ADDITION, DELETION, TRANSLATION,
+		NO_CHANGE, REFLECTED, ROTATED, SCALED, FILL, ADDITION, DELETION, TRANSLATION,
 	}
 }
